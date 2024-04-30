a34b3ca53e44a28788ad735a4e6d0b0701ac6823
Jair Francisco <jairfrancisco2018@gmail.com>
2024-04-30 00:47:37 -0300

    Add Header Component

---
+++ app/_components/header.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+    return (
+        <div className="flex justify-between pt-6 px-5">
+            <Image src="/logo.png" alt="FSW IFood" height={30} width={140} />
+            <Button size="icon" variant="outline" className="bg-transparent border-none">
+                <MenuIcon size={24} />
+            </Button>
+        </div>
+    );
+}
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
+import Header from "./_components/header";
+
 export default function Home() {
   return (
-    <div className="bg-red-600 px-4">
-      <h1>SUPPPPP</h1>
-    </div>
+    <Header />
   );
 }

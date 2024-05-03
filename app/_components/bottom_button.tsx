dd5a43cc3e2644c1a818ff1d9b4d0424eacceda5
Jair Francisco <jairfrancisco2018@gmail.com>
2024-05-03 01:50:57 -0300

    Add: Product Page Found By ID

---
+++ app/_components/bottom_button.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Button } from "./ui/button";
+
+interface BottomButtonProps {
+  text: string;
+}
+
+const BottomButton = ({ text }: BottomButtonProps) => {
+  return (
+    <div className="fixed bottom-0 w-full rotate-180 transform rounded-b-3xl bg-white px-5 py-5 shadow-md">
+      <Button size={"lg"} className="w-full rotate-180">
+        <span className="text-white">{text}</span>
+      </Button>
+    </div>
+  );
+};
+
+export default BottomButton;
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import { db } from "../../_lib/prisma";
 import { notFound } from "next/navigation";
+import ProductImage from "./_components/product_image";
+import { formatPrice } from "@/app/_lib/utils";
+import { AlarmClockIcon, ArrowDownIcon, BikeIcon } from "lucide-react";
+import ProductCount from "./_components/product_count";
+import BottomButton from "@/app/_components/bottom_button";
+import { Card } from "@/app/_components/ui/card";
 
 interface ProductPageProps {
   params: {
@@ -13,20 +19,106 @@ const ProductPage = async ({ params: { id } }: ProductPageProps) => {
     where: {
       id,
     },
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+          imageUrl: true,
+          deliveryFee: true,
+          deliveryTimeMinutes: true,
+        },
+      },
+    },
   });
 
   if (!product) return notFound();
 
   return (
-    <div>
-      <h1>Product {product.name}</h1>
-      <Image
-        src={product.imageUrl}
-        alt={product?.name}
-        width={150}
-        height={150}
-      />
-    </div>
+    <>
+      <div className="pb-20">
+        {/* IMAGEM */}
+        <ProductImage product={product} />
+
+        {/* INFO */}
+        <div className="p-5">
+          {/* RESTAURANT */}
+          <div className="flex items-center gap-2">
+            <div className="relative h-6 w-6">
+              <Image
+                src={product.restaurant.imageUrl}
+                alt={product.restaurant.name}
+                fill
+                quality={100}
+                className="rounded-full object-cover shadow-sm"
+              />
+            </div>
+            <span className="text-sm">{product.restaurant.name}</span>
+          </div>
+
+          {/* PRODUTO */}
+          <div className="py-3 text-2xl font-bold">{product.name}</div>
+
+          {/* PREÇO + QUANTIDADE*/}
+          <div className="flex flex-row items-center justify-between">
+            <div className="flex flex-col">
+              <div className="flex flex-row items-center gap-4 text-2xl font-bold">
+                {formatPrice(Number(product.price), product.discountPercentage)}
+                {product.discountPercentage > 0 && (
+                  <div className="flex items-center gap-0.5 rounded-full bg-primary px-2 text-sm text-white">
+                    <ArrowDownIcon size={14} />
+                    {product.discountPercentage}%
+                  </div>
+                )}
+              </div>
+              {product.discountPercentage > 0 && (
+                <span className="text-sm text-muted-foreground">
+                  De: {formatPrice(Number(product.price))}
+                </span>
+              )}
+            </div>
+
+            <ProductCount />
+          </div>
+
+          {/* ENTREGA */}
+          <Card className="mt-7 flex flex-row items-center justify-around rounded-md border border-solid border-gray-200 py-7 shadow-md">
+            {/* CUSTO */}
+            <div className="flex-col">
+              <div className="flex items-center gap-2">
+                Entrega
+                <BikeIcon size={16} className="text-primary" />
+              </div>
+              <div className="text-center font-bold">
+                {Number(product.restaurant.deliveryFee) > 0 ? (
+                  <p>R${Number(product.restaurant.deliveryFee)},00</p>
+                ) : (
+                  "Grátis"
+                )}
+              </div>
+            </div>
+
+            {/* TEMPO */}
+            <div className="flex-col">
+              <div className="flex items-center gap-2">
+                Entrega
+                <AlarmClockIcon size={16} className="text-primary" />
+              </div>
+              <div className="text-center font-bold">
+                {product.restaurant.deliveryTimeMinutes} min
+              </div>
+            </div>
+          </Card>
+
+          {/* DESCRIÇÃO */}
+          <div>
+            <h2 className="pb-3 pt-7 text-lg font-semibold">Sobre</h2>
+            <p className="text-muted-foreground">{product.description}</p>
+          </div>
+        </div>
+      </div>
+
+      <BottomButton text="Adicionar à Sacola" />
+    </>
   );
 };
 

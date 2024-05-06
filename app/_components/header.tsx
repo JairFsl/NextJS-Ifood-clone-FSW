285cdfb7ab7c3d7fa062e866574c1a2e61b593fa
Jair Francisco <jairfrancisco2018@gmail.com>
2024-05-06 18:11:26 -0300

    Add: Click on Logo Navigate Back to Home Page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,24 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
-    return (
-        <div className="flex justify-between pt-6 px-5">
-            <Image src="/logo.png" alt="FSW IFood" height={30} width={140} />
-            <Button size="icon" variant="outline" className="bg-transparent border-none">
-                <MenuIcon />
-            </Button>
-        </div>
-    );
-}
+  return (
+    <div className="flex justify-between px-5 pt-6">
+      <Link href={"/"}>
+        <Image src="/logo.png" alt="FSW IFood" height={30} width={140} />
+      </Link>
+
+      <Button
+        size="icon"
+        variant="outline"
+        className="border-none bg-transparent"
+      >
+        <MenuIcon />
+      </Button>
+    </div>
+  );
+};
 
 export default Header;

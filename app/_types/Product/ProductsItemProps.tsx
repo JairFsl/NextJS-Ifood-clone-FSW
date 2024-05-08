81293befa2565cb0881c257ff4432947ecbc5924
Jair Francisco <jairfrancisco2018@gmail.com>
2024-05-08 18:17:18 -0300

    Refactor: Product and Restaurant Types

--- app/_types/Product/ProductsItemProps.tsx
+++ app/_types/Product/ProductsItemProps.tsx
@@ -3,3 +3,7 @@ import { Restaurant, Product } from "@prisma/client";
 export interface ProductsItemProps extends Product {
   restaurant: Restaurant;
 }
+
+export interface CartProduct extends ProductsItemProps {
+  quantity: number;
+}
